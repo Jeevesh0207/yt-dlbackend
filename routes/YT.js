a36b2f713ed6458f506eb7ed859b78139035a7fa
Jeevesh0207 <r27abbit@gmail.com>
2024-01-25 21:54:40 +0530

    Modify Search

--- routes/YT.js
+++ routes/YT.js
@@ -1,45 +1,57 @@
-const express=require('express')
+const express = require('express')
 const ytdl = require('ytdl-core')
-const YT=express.Router()
+const YT = express.Router()
 
-YT.post('/',async(req,res)=>{
+YT.post('/', async (req, res) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    const ID=req.body.ID
-    const Data={
-        title:"",
-        thumbnails:"",
-        duration:"",
-        videoformats:[],
-        audioformats:[],
-        defaultaudio:"",
+    const URL = req.body.URL
+    const Data = {
+        title: "",
+        thumbnails: "",
+        duration: "",
+        videoformats: [],
+        audioformats: [],
+        defaultaudio: "",
     }
-    const info=await ytdl.getInfo(ID)
-    const Title=info.videoDetails.title
-    const Thumbnail=info.videoDetails.thumbnails
-    const Len=Thumbnail.length
-    const Audio=ytdl.chooseFormat(info.formats,{quality:'highestaudio'})
-    Data.defaultaudio=Audio.url
-    Data.duration=info.videoDetails.lengthSeconds
-    Data.title=Title
-    Data.thumbnails=Thumbnail[Len-1].url
-    const AllData=info.formats
-    AllData.map((item)=>{
-        if(item.mimeType.includes('video')){
-          if(item.contentLength){
-            Data.videoformats.push({qualityLabel:item.qualityLabel,url:item.url,contentLength:item.contentLength})
-          }
+    let ID = ""
+    try {
+        ID = ytdl.getURLVideoID(URL)
+    } catch (err) {
+        const regex = /(youtu.be\/|youtube.com\/(watch\?(.*&)?v=|(embed|v|shorts|live)\/))([^\?&"'>]+)/;
+        const matches = URL.match(regex);
+        ID = (matches ? matches[5] : null);
+    }
+    if(ID===null) {
+        res.send("ERROR")
+        res.end()
+    }
+    const info = await ytdl.getInfo(ID)
+    const Title = info.videoDetails.title
+    const Thumbnail = info.videoDetails.thumbnails
+    const Len = Thumbnail.length
+    const Audio = ytdl.chooseFormat(info.formats, { quality: 'highestaudio' })
+    Data.defaultaudio = Audio.url
+    Data.duration = info.videoDetails.lengthSeconds
+    Data.title = Title
+    Data.thumbnails = Thumbnail[Len - 1].url
+    const AllData = info.formats
+    AllData.map((item) => {
+        if (item.mimeType.includes('video')) {
+            if (item.contentLength) {
+                Data.videoformats.push({ qualityLabel: item.qualityLabel, url: item.url, contentLength: item.contentLength })
+            }
         }
-        if(item.mimeType.includes('audio')){
-            if(item.contentLength){
-                Data.audioformats.push({url:item.url,contentLength:item.contentLength})
+        if (item.mimeType.includes('audio')) {
+            if (item.contentLength) {
+                Data.audioformats.push({ url: item.url, contentLength: item.contentLength })
             }
         }
     })
     Data.videoformats.sort((a, b) => b.contentLength - a.contentLength);
     Data.audioformats.sort((a, b) => b.contentLength - a.contentLength);
     res.send(Data)
-    // res.end()
+    res.end()
 })
 
-module.exports=YT
+module.exports = YT
